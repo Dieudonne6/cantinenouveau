8d029cb37985840e5d015194ea22158941c8e4b1
Nourou-Dev <amadounouroudine1@gmail.com>
2024-08-13 15:55:01 +0100

    nnourou

--- resources/views/pages/etat.blade.php
+++ resources/views/pages/etat.blade.php
@@ -23,11 +23,11 @@
                                 <div class="form-group row">
                                     <div class="col">
                                         <label for="debut">Du</label>
-                                        <input name="debut" id="debut" type="date" class="typeaheads" required>
+                                        <input name="debut" id="debut" type="date" value="{{ old('date', now()->subMonths(3)->format('Y-m-d')) }}" class="typeaheads" required>
                                     </div>
                                     <div class="col">
                                         <label for="fin">Au</label>
-                                        <input name="fin" id="fin" type="date" class="typeaheads" required>
+                                        <input name="fin" id="fin" type="date" value="{{ old('date', now()->format('Y-m-d')) }}" class="typeaheads" required>
                                     </div>
                     
                                     <div class="col">
