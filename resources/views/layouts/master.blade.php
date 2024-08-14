bce66de51102fa49f2419928aaa999982033910f
Pascaline Dah Zinsou <pascaline@wazoom-studio.com>
2024-08-14 09:24:33 +0100

    Made somes change.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,10 +10,10 @@
   <!-- base:css -->
   <link rel="stylesheet" href="{{asset('assets/vendors/typicons/typicons.css')}}">
   <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="{{asset('assets/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
   <script src="{{ asset('davidshimjs-qrcodejs-04f46c6/qrcode.js') }}"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="{{asset('assets/all.min.css')}}">
   <link rel="stylesheet" href="https://cdn.datatables.net/2.0.7/css/dataTables.dataTables.css" />
   <!-- endinject -->
   <!-- plugin css for this page -->
